c5b9471efd6a4ceee64fd31fdbe4d3a5b4d1e984
Rupa <aennamswaroopa@yahoo.com>
2019-02-19 15:56:57 -0800

    Updates

---
+++ precache-manifest.374af675eaa83e725ac64b54471c7a16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e32d115b4094aac1eca",
+    "url": "/sh-cart/static/js/runtime~main.9e32d115.js"
+  },
+  {
+    "revision": "a3c7c2cef68a1742bfb1",
+    "url": "/sh-cart/static/js/main.a3c7c2ce.chunk.js"
+  },
+  {
+    "revision": "e65d2ef46aa8be294d01",
+    "url": "/sh-cart/static/js/2.e65d2ef4.chunk.js"
+  },
+  {
+    "revision": "a3c7c2cef68a1742bfb1",
+    "url": "/sh-cart/static/css/main.05303472.chunk.css"
+  },
+  {
+    "revision": "118151f2b0d7455cdca5486e4f2543c2",
+    "url": "/sh-cart/index.html"
+  }
+];
